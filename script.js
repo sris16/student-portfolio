b140904fce82ff0382cf03eb4758fb5c65a28f38
sris16 <srisakthi7890@gmail.com>
2025-08-20 19:59:00 +0530

    Added Portfolio Files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple smooth scroll for navigation
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+  });
+});
